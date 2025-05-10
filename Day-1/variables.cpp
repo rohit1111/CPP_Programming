902eb51a9420f7789fe67d747aad8fe8eff30a46
rohit1111 <rohit.jagtap10@gmail.com>
2025-05-11 00:43:29 +0530

    Create variables.cpp

---
+++ Day-1/variables.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a;										//Step-1
+	char ch;									//Step-1
+	float f;									//Step-1
+	double d;									//Step-1
+	bool b;										//Step-1
+	//cout<<"Size of int: "<<sizeof(a);			//Step-1
+	//cout<<"Size of char: "<<sizeof(ch);			//Step-1
+	//cout<<"Size of float: "<<sizeof(f);			//Step-1
+	//cout<<"Size of double: "<<sizeof(d);		//Step-1
+	//cout<<"Size of bool: "<<sizeof(b);			//Step-1
+	cout<<"Size of int: "<<sizeof(a) << endl;			//Step-2
+	cout<<"Size of char: "<<sizeof(ch) << endl;			//Step-2
+	cout<<"Size of float: "<<sizeof(f) << "\n";			//Step-2
+	cout<<"Size of double: "<<sizeof(d) << "\n";			//Step-2
+	cout<<"Size of bool: "<<sizeof(b) << endl;			//Step-2
+	return 0;
+}
