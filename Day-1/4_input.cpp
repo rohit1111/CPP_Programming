fe5b7f9e8fcb4af01328cad56d3936124cdd7adb
rohit1111 <rohit.jagtap10@gmail.com>
2025-05-11 01:16:30 +0530

    Create 4_input.cpp

---
+++ Day-1/4_input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a=10;
+	char ch='b';
+	float f =4.5;
+	double d = 5.5;
+
+	cout<<"a: "<<a <<" ch:" << ch <<" f:"<<f<<" d:"<<d<<endl;
+
+	cin>>a>>ch>>f>>d;
+
+	cout<<"a: "<<a <<" ch:" << ch <<" f:"<<f<<" d:"<<d<<endl;
+
+	return 0;
+}
