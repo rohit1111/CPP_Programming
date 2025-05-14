9429e85abc5be892da31cc07748368a298d627f1
rohit1111 <rohit.jagtap10@gmail.com>
2025-05-15 00:39:56 +0530

    Create 7_while_loop.cpp

---
+++ Day-2/7_while_loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Learn: While & do while loop
+
+	int i=-1;
+	while(i++ < 5){
+		cout<<i<<endl;
+	}
+
+	i = 8;
+
+	do{
+		cout<<i<<endl;
+	}while(i<5);
+	return 0;
+}
