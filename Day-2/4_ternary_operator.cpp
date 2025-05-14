91f8bfe8608d88116feece90740464d192431287
rohit1111 <rohit.jagtap10@gmail.com>
2025-05-15 00:22:45 +0530

    Create 4_ternary_operator.cpp

---
+++ Day-2/4_ternary_operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Learn: Terneary Operator
+	int marks;
+	cin>>marks;
+
+	(marks >= 33) ? cout<<"Pass"<<endl : cout<<"Fail"<<endl;
+
+	return 0;
+}
