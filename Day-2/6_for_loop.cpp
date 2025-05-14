c4ca00d149336660e4d29e55ae462c43fcf73134
rohit1111 <rohit.jagtap10@gmail.com>
2025-05-15 00:36:21 +0530

    Create 6_for_loop.cpp

---
+++ Day-2/6_for_loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Learn: For loop
+	for(int i=0;i<=5;i++){
+		cout<<i<<endl;
+	}
+
+	int j=0;
+	for(;j<=5;j+=2){
+		cout<<j<<endl;
+	}
+
+	return 0;
+}
