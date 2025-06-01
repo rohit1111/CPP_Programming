bcb736f325603c7d521887f233156c63e272a259
rohit1111 <rohit.jagtap10@gmail.com>
2025-06-01 12:43:41 +0530

    Create function2_productofdigits.cpp

---
+++ Day-3/function2_productofdigits.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int number);
+int productOfDigits(int number);
+
+int main(){
+	int number;
+	cin>>number;
+
+	int result = sumOfDigits(number);
+	cout<<"Number: "<< number << ", & Sum Of Digits: "<<result<<endl;
+	result = productOfDigits(number);
+	cout<<"Number: "<< number << ", & Product Of Digits: "<<result<<endl;
+	return 0;
+}
+
+int sumOfDigits(int number){
+	int result = 0;
+	while(number>0){
+		result += number%10;
+		number /=10;
+	}
+	return result;
+}
+int productOfDigits(int number){
+	int result = 1;
+	while(number>0){
+		result *= number%10;
+		number /= 10;
+	}
+	return result;
+}
