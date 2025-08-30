e7fc5a5fcfa6e854eff7a0f13df6865294f1a88a
rohit1111 <rohit.jagtap10@gmail.com>
2025-08-30 23:01:52 +0530

    Create deque7_length_of_longest_substr.cpp

---
+++ Queue/Deque/deque7_length_of_longest_substr.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+class Solution {
+  public:
+    int longestUniqueSubstring(string &s) {
+		deque<char> q;
+		int freq[26] = {};
+		int answer = 0;
+		for(int i=0;i<s.size();i++){
+			freq[s[i] - 'a']++;
+			q.push_back(s[i]);
+			while(!q.empty() && freq[s[i] - 'a'] > 1){
+				freq[q.front() - 'a']--;
+				q.pop_front();
+			}
+			answer = max(answer,(int)q.size());
+		}
+		return answer;
+    }
+};
+int main() {
+	/*
+	Using Deque
+	Length of the longest substring: https://www.geeksforgeeks.org/problems/length-of-the-longest-substring3036/1
+	Given a string s, find the length of the longest substring without repeating characters.
+
+	Examples:
+
+	Input: s = "geeksforgeeks"
+	Output: 7
+	Explanation: Longest substring is "eksforg".
+	*/
+	Solution* s1 = new Solution();
+	string s = "geeksforgeeks";
+	cout<<s1->longestUniqueSubstring(s);
+	return 0;
+}
