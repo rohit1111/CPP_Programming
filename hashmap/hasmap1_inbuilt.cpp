0e3575238e2ae70b48a048a04b5efed432ecf2bf
rohit1111 <rohit.jagtap10@gmail.com>
2025-09-03 00:01:57 +0530

    Create hasmap1_inbuilt.cpp

---
+++ hashmap/hasmap1_inbuilt.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main() {
+	vector<int> arr = {1,2,8,1};
+	map<int,int> m;
+	int n = arr.size();
+	for(int i=0;i<n;i++){
+		m[arr[i]]++;
+	}
+	cout<<m[1]<<" ";
+	cout<<m[2]<<" ";
+	cout<<m[8]<<" ";
+
+	return 0;
+}
